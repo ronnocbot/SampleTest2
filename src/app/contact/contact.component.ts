3a6e22f356da4f342caa1ca5aefd6e1890814bc1
ronnocbot <ronnocbot@gmail.com>
2020-05-06 18:38:30 -0700

    finish exam

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,6 +5,18 @@ import { LocalStorageService } from '../localStorageService';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
+import { consumeBinding } from '@angular/core/src/render3/instructions';
+import { HttpClient } from '@angular/common/http';
+import { NullViewportScroller } from '@angular/common/src/viewport_scroller';
+
+export interface IContact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  owed: number;
+}
 
 @Component({
   selector: 'app-contact',
@@ -13,7 +25,8 @@ import { ToastService } from '../toast/toast.service';
 })
 export class ContactComponent implements OnInit {
 
-  contacts: Array<any> = [];
+  contacts: Array<IContact> = [];
+  params: string;
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
@@ -22,8 +35,78 @@ export class ContactComponent implements OnInit {
   ) {
   }
 
-  async ngOnInit() {
-
+  async loadContacts() {
+    let contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts && contacts.length > 0) {
+      // contacts = contacts;
+    } else {
+      contacts = await this.loadContactsFromJson();
+    }
+    console.log('this.contacts from ngOninit...', this.contacts);
+    this.contacts = contacts;
+    return contacts;
   }
+
+  async ngOnInit() {
+    this.contacts = await this.loadContacts();
+  }
+
+  async loadContactsFromJson() {
+    const contacts = await this.http.get('assets/contacts.json').toPromise();
+    return contacts.json();
+  }
+
+  addContact() {
+    const contact: IContact = {
+      id: null,
+      firstName: null,
+      lastName: null,
+      email: null,
+      phone: null,
+      owed: null
+    };
+    this.contacts.unshift(contact);
+    this.saveToLocalStorage();
+  }
+
+  deleteContact(index: number) {
+    this.contacts.slice(index, 1);
+    this.saveToLocalStorage();
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('contacts', JSON.stringify(this.contacts));
+  }
+
+  calculate() {
+    let owed = 0;
+    for (let i = 0; i < this.contacts.length; i++) {
+      // console.log('i--->', i, 'this.contacts[i]', this.contacts[i]);
+      owed += this.contacts[i].owed;
+      // console.log('owed --->', owed);
+    }
+    return {
+      numberofContacts: this.contacts.length,
+      subTotal: owed,
+      taxAmount: owed * .10,
+      total: owed + (owed * .10),
+    };
+  }
+
+  finalize() {
+    console.log('from finalize....');
+    const data = this.calculate();
+    localStorage.setItem('calculatedData', JSON.stringify(data));
+    this.router.navigate(['home', data]);
+  }
+
+  search(params: string) {
+    console.log('from search.... params', params);
+
+    this.contacts = this.contacts.filter((contact: IContact) => {
+      return contact.firstName.toLowerCase() === params.toLowerCase();
+    });
+  }
+
 
 }
